cabe6139a23ece8b1d64c991ab6e35cbcc96b784
abdul <malik301002@gmail.com>
2022-08-30 21:28:53 +0700

    feat: add MantineProvider to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
+      <App />
+    </MantineProvider>
+  </StrictMode>
 );
