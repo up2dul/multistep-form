dfb124db69c3a482a137fd4adea7431e2d27d3cf
abdul <malik301002@gmail.com>
2022-08-31 20:15:59 +0700

    feat: edit first step of the form

--- src/components/step-first.jsx
+++ src/components/step-first.jsx
@@ -1,22 +1,13 @@
-import { Container, Select, TextInput, Title } from '@mantine/core';
+import { Container, TextInput, Title } from '@mantine/core';
 
 export function StepFirst() {
   return (
     <Container mt={30} size='xs'>
-      <Title order={2} mb={20}>
-        👀 Let us know bit about you
+      <Title order={2} mb={25}>
+        👀 What&apos;s your name?
       </Title>
-      <TextInput mb={20} label='First name' placeholder='e.g. Thomas' withAsterisk />
-      <TextInput mb={20} label='Last name' placeholder='e.g. Basir' />
-      <Select
-        label='Your gender'
-        placeholder='Pick one'
-        data={[
-          { value: 'male', label: 'Male' },
-          { value: 'female', label: 'Female' }
-        ]}
-        withAsterisk
-      />
+      <TextInput mb={25} label='First name' placeholder='e.g. Thomas' withAsterisk />
+      <TextInput mb={25} label='Last name' placeholder='e.g. Basir' />
     </Container>
   );
 }
