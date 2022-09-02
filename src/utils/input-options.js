f6b0a431ba065ef9874de63196ca92fa7f21ef57
abdul <malik301002@gmail.com>
2022-09-02 22:59:53 +0700

    typo

--- src/utils/input-options.js
+++ src/utils/input-options.js
@@ -2,13 +2,14 @@ const levelList = ['Newbie', 'Beginner', 'Intermediate', 'Advanced', 'Expert'];
 
 const stackList = ['Frontend', 'Backend', 'Full stack', "I don't know yet"];
 
-const experienceList = ['No experience', '1-3 year', '3-5 years', 'More than 5 years'];
+const experienceList = ['No experience', '1-3 years', '3-5 years', 'More than 5 years'];
 
 const learnList = ['Self learning', 'Bootcamp', 'Formal education', 'Mentorship', 'Other'];
 
 const sourcesInfoList = [
   'Family',
   'Friends',
+  'Facebook',
   'Instagram',
   'Linkedin',
   'Twitter',
