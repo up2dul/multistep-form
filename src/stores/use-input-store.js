87e4df44cddcd26586951f00141a027c736a1901
abdul <malik301002@gmail.com>
2022-09-06 22:02:52 +0700

    feat: edit input-store states

--- src/stores/use-input-store.js
+++ src/stores/use-input-store.js
@@ -1,10 +1,8 @@
 import create from 'zustand';
 
-export const useInputStore = create((set) => ({
-  inputs: {
-    firstStep: null,
-    secondStep: null,
-    thirdStep: null
-  },
-  setFirstStep: () => set((state) => console.log(state))
+export const useFormStore = create((set) => ({
+  firstForm: null,
+  secondStep: null,
+  thirdStep: null,
+  setFirstForm: (data) => set(() => ({ firstForm: data }))
 }));
