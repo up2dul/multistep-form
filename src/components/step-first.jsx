96b8951e2af53f429946840f01a9e211e3d09778
abdul <malik301002@gmail.com>
2022-09-06 22:12:49 +0700

    feat: applied zustand and react-hook-form to first step form

--- src/components/step-first.jsx
+++ src/components/step-first.jsx
@@ -1,13 +1,35 @@
-import { Container, TextInput, Title } from '@mantine/core';
+import { useForm } from 'react-hook-form';
+import { Button, Container, TextInput, Title } from '@mantine/core';
+
+import { useFormStore, useStepStore } from '@/stores';
+import { firstTextList } from '@/utils';
+import { StepperLayout } from '@/components';
 
 export function StepFirst() {
+  const { setNextStep } = useStepStore((state) => state);
+
+  const { firstForm, setFirstForm } = useFormStore((state) => state);
+
+  const { register, handleSubmit } = useForm({ defaultValues: firstForm || {} });
+
+  const onSubmit = (data) => {
+    setFirstForm(data);
+    setNextStep();
+  };
+
   return (
     <Container mt={30} size='xs'>
       <Title order={2} mb={25}>
         👀 What&apos;s your name?
       </Title>
-      <TextInput mb={25} label='First name' placeholder='e.g. Guntur' withAsterisk />
-      <TextInput mb={25} label='Last name' placeholder='e.g. Hidayat' />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {firstTextList.map((props, idx) => (
+          <TextInput key={idx} mb={25} {...props} {...register(props.name)} />
+        ))}
+        <StepperLayout>
+          <Button type='submit'>Next</Button>
+        </StepperLayout>
+      </form>
     </Container>
   );
 }
