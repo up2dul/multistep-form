a3627016a88e4fb487e1ea958471f3f034036308
abdul <malik301002@gmail.com>
2022-09-06 22:15:08 +0700

    feat: applied zustand and react-hook-form to second step form

--- src/components/step-second.jsx
+++ src/components/step-second.jsx
@@ -1,77 +1,54 @@
-import { Container, Group, NumberInput, Radio, TextInput, Title } from '@mantine/core';
+import { useForm } from 'react-hook-form';
+import { Button, Container, Group, Radio, TextInput, Title } from '@mantine/core';
 import { DatePicker } from '@mantine/dates';
-import {
-  IoCalendarNumber,
-  IoHome,
-  IoCall,
-  IoMail,
-  IoGlobeOutline,
-  IoLogoGithub,
-  IoLogoLinkedin
-} from 'react-icons/io5';
+import { IoCalendarNumber } from 'react-icons/io5';
+
+import { useFormStore, useStepStore } from '@/stores';
+import { secondTextList } from '@/utils';
+import { StepperLayout } from '@/components';
 
 export function StepSecond() {
+  const { register, handleSubmit } = useForm();
+
+  const { setPrevStep, setNextStep } = useStepStore((state) => state);
+
+  const { firstName } = useFormStore((state) => state.firstForm);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    setNextStep();
+  };
+
   return (
     <Container mt={30} size='xs'>
       <Title order={2} mb={25}>
-        👋 Hi Guntur, <br /> Let us know about your info and contact
+        👋 Hi {firstName}, <br /> Let us know about your info and contact
       </Title>
-      <Group mb={25} grow>
-        <Radio.Group label='Gender' orientation='vertical' withAsterisk>
-          <Radio value='male' label='Male' />
-          <Radio value='female' label='Female' />
-        </Radio.Group>
-        <DatePicker
-          icon={<IoCalendarNumber />}
-          label='Birth date'
-          placeholder='Pick date'
-          dropdownType='modal'
-          withAsterisk
-        />
-      </Group>
-      <TextInput
-        mb={25}
-        icon={<IoHome />}
-        label='City and Country'
-        placeholder='e.g. Bandung, Indonesia'
-        withAsterisk
-      />
-      <NumberInput
-        mb={25}
-        icon={<IoCall />}
-        label='Phone number'
-        placeholder='e.g. +62-888-1234-1234'
-        description='Preferably that is used for Telegram/WhatsApp'
-        withAsterisk
-        hideControls
-      />
-      <TextInput
-        mb={25}
-        icon={<IoMail />}
-        label='Email address'
-        placeholder='e.g. guntur@gmail.com'
-        description='Must be an active email'
-        withAsterisk
-      />
-      <TextInput
-        mb={25}
-        icon={<IoGlobeOutline />}
-        label='Personal website'
-        placeholder='e.g. https://guntur.com'
-        description='Optional, but it would be better if you had one'
-      />
-      <TextInput
-        mb={25}
-        icon={<IoLogoGithub />}
-        label='Github URL'
-        placeholder='e.g. https://github.com/guntur'
-      />
-      <TextInput
-        mb={25}
-        icon={<IoLogoLinkedin />}
-        label='Linkedin URL'
-        placeholder='e.g. https://linkedin.com/in/guntur'
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Group mb={25} grow>
+          <Radio.Group label='Gender' orientation='vertical' withAsterisk {...register('gender')}>
+            <Radio value='male' label='Male' />
+            <Radio value='female' label='Female' />
+          </Radio.Group>
+          <DatePicker
+            icon={<IoCalendarNumber />}
+            label='Birth date'
+            placeholder='Pick date'
+            dropdownType='modal'
+            withAsterisk
+            {...register('birthDate')}
+          />
+        </Group>
+        {secondTextList.map((props, idx) => (
+          <TextInput key={idx} mb={25} {...props} {...register(props.name)} />
+        ))}
+        <StepperLayout>
+          <Button variant='default' onClick={setPrevStep}>
+            Back
+          </Button>
+          <Button type='submit'>Next</Button>
+        </StepperLayout>
+      </form>
     </Container>
   );
 }
