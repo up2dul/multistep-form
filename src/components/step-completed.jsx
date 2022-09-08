f82b45547075371dcccd70ac89d82bf390dfafb4
abdul <malik301002@gmail.com>
2022-09-08 14:16:37 +0700

    feat: add stepper button to completed step

--- src/components/step-completed.jsx
+++ src/components/step-completed.jsx
@@ -1,13 +1,28 @@
-import { Container, Title } from '@mantine/core';
+import { Button, Container, Text, Title } from '@mantine/core';
+
+import { StepperLayout } from '@/components';
+import { useStepStore } from '@/store';
 
 export function StepCompleted() {
+  const { setPrevStep } = useStepStore((state) => state);
+
   return (
     <Container mt={30} size='s'>
-      <Title order={2} mb={25}>
-        ✅ All completed, <br />
-        Make sure you&apos;ve filled in the form correctly. <br />
-        And then, you can submit it.
+      <Title order={2} mb={15}>
+        ✅ All completed
       </Title>
+
+      <Text size='lg'>
+        Make sure you&apos;ve filled in the form correctly, <br />
+        And then you can submit it.
+      </Text>
+
+      <StepperLayout>
+        <Button variant='default' onClick={setPrevStep}>
+          Back
+        </Button>
+        <Button type='submit'>Submit</Button>
+      </StepperLayout>
     </Container>
   );
 }
