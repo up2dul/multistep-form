aa41ab7840d21ece1ddb3e66d48e6acdd7dcd324
abdul <malik301002@gmail.com>
2022-09-12 14:29:17 +0700

    feat: add footer and useStyle hook to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,39 @@ import { useEffect } from 'react';
 import { Container, Stepper } from '@mantine/core';
 
 import { useStepStore } from '@/store';
-import { StepCompleted, StepFirst, StepSecond, StepThird } from '@/components';
+import { useStyle } from '@/hooks';
+import { StepCompleted, StepFirst, StepSecond, StepThird, Footer } from '@/components';
 
 function App() {
   const { step } = useStepStore((state) => state);
+
+  const { classes } = useStyle();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [step]);
 
   return (
-    <Container my={60}>
-      <Stepper active={step} breakpoint='sm'>
-        <Stepper.Step label='First step' description='Your name'>
-          <StepFirst />
-        </Stepper.Step>
-        <Stepper.Step label='Second step' description='Your details info'>
-          <StepSecond />
-        </Stepper.Step>
-        <Stepper.Step label='Third step' description='More about you'>
-          <StepThird />
-        </Stepper.Step>
-        <Stepper.Completed>
-          <StepCompleted />
-        </Stepper.Completed>
-      </Stepper>
-    </Container>
+    <main className={classes.main}>
+      <Container pt={60} pb={120}>
+        <Stepper active={step} breakpoint='sm'>
+          <Stepper.Step label='First step' description='Your name'>
+            <StepFirst />
+          </Stepper.Step>
+          <Stepper.Step label='Second step' description='Your details info'>
+            <StepSecond />
+          </Stepper.Step>
+          <Stepper.Step label='Third step' description='More about you'>
+            <StepThird />
+          </Stepper.Step>
+          <Stepper.Completed>
+            <StepCompleted />
+          </Stepper.Completed>
+        </Stepper>
+      </Container>
+
+      <Footer />
+    </main>
   );
 }
 
